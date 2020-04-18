8be845b92e9f8beb382e6b74e7bd0c03ea77a140
Suriyadeepan Ramamoorthy <suriyadeepan.r@gmail.com>
2020-04-18 07:54:17 +0530

    highlight -> highlighter; fixes highlighting issue; works now

--- scripts/highlighter.js
+++ scripts/highlighter.js
@@ -221,7 +221,7 @@
 
         if (index >= 0) { 
             innerHTML = innerHTML.substring(0,index) 
-                + "<span class='highlight'>" 
+                + "<span class='highlighter'>" 
                 + innerHTML.substring(index,index+text.length)
                 + "</span>"
                 + innerHTML.substring(index + text.length);
